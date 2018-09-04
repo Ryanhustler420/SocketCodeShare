693cb1f2b574ad44f6c14329d287280ab160334e
Gaurav Gupta <gouravgupta840@gmail.com>
2018-09-05 00:35:04 +0530

    Creating Contact Page for Contact us and redirect to thankuPage with routing

--- SocketCodeShare/routes/index.js
+++ SocketCodeShare/routes/index.js
@@ -11,4 +11,13 @@ router.get('/about',function(req,res,next){
   res.render('about',{ title: 'codeShare - a platform for sharing code. created by GauravGupta!'});
 });
 
+//this route is for two operation first user will get this page and after submistion of the
+//form he/she will redirect to tanku page.
+router.route('/contact')
+  .get(function(req,res,next){
+    res.render('contact',{title:'codeShare - a platform for sharing code. created by GauravGupta!'});
+  }).post(function(req,res,next){
+    res.render('thankYou',{title:'codeShare - a platform for sharing code. created by GauravGupta!'});
+  });
+
 module.exports = router;
