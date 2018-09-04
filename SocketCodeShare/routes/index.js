525b607bc6ad64fa404750c8d84d06fbb47a4437
Gaurav Gupta <gouravgupta840@gmail.com>
2018-09-04 22:44:47 +0530

    Creating our first Static page and set links respective to them

--- SocketCodeShare/routes/index.js
+++ SocketCodeShare/routes/index.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'codeShare - a platform for sharing code. created by GauravGupta!' });
+});
+
+
+router.get('/about',function(req,res,next){
+  res.render('about',{ title: 'codeShare - a platform for sharing code. created by GauravGupta!'});
 });
 
 module.exports = router;
